d954ade9ffe7fa93bb96a6cb184b9d91a802be68
Junyu Yang <prowessyang@gmail.com>
2022-03-15 04:18:13 +0000

    nft trades validated works

---
+++ adhoc queries/validate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import snowflake.connector
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user='yangjunyu',
+    password='********',
+    account='ouhduii-zs68033'
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
--- backfill.py
+++ backfill.py
@@ -8,6 +8,28 @@ import etl_utls as utl
 # for date in date_gaps:
 #     up.update_nft_trade_opensea(date, use_upsert=False)
 
+# date_gaps = utl.check_table_for_date_gaps(table="eth_token_transfers_2022", start_date="2022-01-01")
+# for date in date_gaps:
+#     up.update_token_transfers(date, running_in_cloud=utl.RUNNING_IN_CLOUD, use_upsert=False)
+
+
+################  Three source tables  ################
+# transactions
+date_gaps = utl.check_table_for_date_gaps(table="eth_transactions", start_date="2022-01-01")
+for date in date_gaps:
+    up.update_eth_transactions(date)
+
+# contracts
+up.update_contracts()
+
+# trades
+# TODO don't hardcode this
+date_gaps = utl.check_table_for_date_gaps(table="nft_trades", start_date="2022-01-01")
+for date in date_gaps:
+    up.update_nft_trade_opensea(date, use_upsert=False)
+
+# token transfers
+# TODO don't hardcode this
 date_gaps = utl.check_table_for_date_gaps(table="eth_token_transfers_2022", start_date="2022-01-01")
 for date in date_gaps:
     up.update_token_transfers(date, running_in_cloud=utl.RUNNING_IN_CLOUD, use_upsert=False)
